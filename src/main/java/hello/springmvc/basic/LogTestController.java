f580d6c4cca6eb4d68fa319fbc62d66446efd359
정상화 <jungsh930210@gmail.com>
2023-01-12 22:27:49 +0900

    init

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @Slf4j
+// @Slf4j // Lombokß
 @RestController // REST-API에 사용, HTTP BODY에 String을 넣어 화면에 그대로 반환함 (간단한 테스트라 여기에 사용)
 public class LogTestController {
 

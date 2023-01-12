85422acb115592a21d597c1fd829098c30c7c1df
SanghwaJ <jungsh930210@gmail.com>
2023-01-12 17:51:26 +0900

    logging

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,31 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Slf4j
+@RestController // REST-API에 사용, HTTP BODY에 String을 넣어 화면에 그대로 반환함 (간단한 테스트라 여기에 사용)
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        // 무조건 출력 (운영에 사용하면 안됨)
+        System.out.println("name = " + name);
+
+        // log 레벨에 따라 출력
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "OK";
+    }
+}

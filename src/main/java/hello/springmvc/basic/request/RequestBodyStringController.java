5362390501ef80e1ae02923ce4393a45b1f32f08
정상화 <jungsh930210@gmail.com>
2023-01-15 23:40:49 +0900

    edit

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -27,13 +27,13 @@ public class RequestBodyStringController {
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
         log.info("messageBody={}", messageBody);
-
         response.getWriter().write("ok");
     }
 
     @PostMapping("/request-body-string-v2")
     public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
         log.info("messageBody={}", messageBody);
         responseWriter.write("ok");
     }
